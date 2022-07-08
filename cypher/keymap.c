75f0a9db057c437bae7cc42f66c68920fc4fccbe
cypher <jayanthsreejith13@gmail.com>
2022-07-09 00:15:24 +0530

    flash-ready v1
    File size: 26558/26624 (99%, 66 bytes free)

--- cypher/keymap.c
+++ cypher/keymap.c
@@ -1,27 +1,25 @@
+//the numbers represent the layers and the codes are assigned based on qmk docs
 #include QMK_KEYBOARD_H
-#define _FIRST 0
-#define _SECOND 1
-#define _THIRD 2
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_FIRST] = LAYOUT_65_ansi(
+    [0] = LAYOUT_65_ansi(
         KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
         CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
         KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-        KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(_SECOND),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
-    [_SECOND] = LAYOUT_65_ansi(
+    [1] = LAYOUT_65_ansi(
         KC_GESC,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_PWR,
         _______,        RGB_TOG, KC_TRNS, KC_TRNS, RGB_RMOD, RGB_MOD, RGB_VAD, RGB_VAI, RGB_SAD, RGB_SAI,RGB_HUD,RGB_HUI,_______,_______,_______, 
         CTL_T(KC_CAPS), _______, _______, _______, _______, _______, _______, _______, RGB_SPD, RGB_SPI, KC_BRID, KC_BRIU,       _______,_______,
         KC_LSFT,               _______, _______, _______, _______, _______, _______,_______, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, _______,_______,
         _______,_______, _______,                            _______,                        _______, _______, KC_MPLY, KC_MUTE, _______, _______
     ),
-    [_THIRD] = LAYOUT_65_ansi(
+    [2] = LAYOUT_65_ansi(
         KC_TRNS,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_PWR,
         _______,        KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2, _______, _______, _______, _______, _______, _______,_______,_______,_______,_______,_______, 
         CTL_T(KC_CAPS), KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______,       _______,_______,
         KC_LSFT,               _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______, KC_MS_WH_UP,_______,
         _______,_______, _______,                            _______,                        _______, _______, KC_MPLY, KC_MUTE, KC_MS_WH_DOWN, _______
-    ),
+    )
 };
